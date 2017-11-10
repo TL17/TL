6c0f992db054ab11297056b3a27720e414018049
Shijian <liushi2@hotmail.com>
2017-11-10 11:01:58 +0800

    添加开放跨域+URL解码

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,6 +3,7 @@ package test;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
 
 @WebServlet(name="HelloWorld", urlPatterns="/HelloWorld/*")
 public class HelloWorld extends javax.servlet.http.HttpServlet {
@@ -11,9 +12,11 @@ public class HelloWorld extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
         String URI = request.getRequestURI();
         int index = URI.lastIndexOf('/');
         String name = URI.substring(index + 1);
+        name = URLDecoder.decode(name,"utf-8");
         PrintWriter out = response.getWriter();
         out.print("{\"msg\":\"Hello " + name + "!\"}");
     }
