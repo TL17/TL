46438ff24b1ad2b97eb1346ecef884d7d136c31b
Shijian <liushi2@hotmail.com>
2017-11-14 16:46:51 +0800

    增加post测试

---
+++ src/apis/SignUp.java
@@ -0,0 +1,17 @@
+package apis;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+
+@WebServlet(name="SignUp", urlPatterns="/apis/sign_up")
+public class SignUp extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -7,17 +7,49 @@ import java.net.URLDecoder;
 
 @WebServlet(name="HelloWorld", urlPatterns="/HelloWorld/*")
 public class HelloWorld extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
-    }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setContentType("text/html;charset=utf-8");
+
         String URI = request.getRequestURI();
         int index = URI.lastIndexOf('/');
         String name = URI.substring(index + 1);
         name = URLDecoder.decode(name,"utf-8");
+
+        String date = request.getParameter("date");
+        if (date == null) {
+            date = "";
+        }
+
+        String outStr = "{" +
+                "\"msg\":\"Hello " + name + "!\"," +
+                "\"date\":\""+ date +"\"" +
+                "}";
         PrintWriter out = response.getWriter();
-        out.print("{\"msg\":\"Hello " + name + "!\"}");
+        out.print(outStr);
     }
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setContentType("text/html;charset=utf-8");
+
+        String URI = request.getRequestURI();
+        int index = URI.lastIndexOf('/');
+        String name = URI.substring(index + 1);
+        name = URLDecoder.decode(name,"utf-8");
+
+        String date = request.getParameter("date");
+        if (date == null) {
+            date = "";
+        }
+
+        String outStr = "{" +
+                "\"msg\":\"Hello " + name + "!\"," +
+                "\"date\":\""+ date +"\"" +
+                "}";
+        PrintWriter out = response.getWriter();
+        out.print(outStr);
+    }
+
 }
