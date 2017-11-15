5416ce7c5adf3b3521bbd2ed0957fd095bc21685
Shijian <liushi2@hotmail.com>
2017-11-15 12:21:20 +0800

    HelloWorld-POST确认，Angular测试页面

--- src/apis/SignUp.java
+++ src/apis/SignUp.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLDecoder;
 
-@WebServlet(name="SignUp", urlPatterns="/apis/sign_up")
+@WebServlet(name="SignUp", urlPatterns="/sign_up")
 public class SignUp extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         
