722f1f8839e2c7286b7fdc97f52d904eac03e090
zukihxy <14302010050@fudan.edu.cn>
2017-11-22 01:32:59 +0800

    search for course, interface wait to be refined

--- web/js/Courses.js
+++ web/js/Courses.js
@@ -7,13 +7,45 @@ app.controller("courses_ctrl", ['$scope', '$rootScope', '$http', function($scope
     //     }
     // };
 
+    $scope.search = function(keyword) {
+        $http.get(serverUrl+"/course/search",{params:{keyword: keyword}})
+            .success(function(ret) {
+                var ele = angular.element(document.querySelector("#coursesList>ul"));
+                ele.empty();
+
+                if (ret.status) {
+                    angular.forEach(ret.courses, function(c) {
+                        // alert(c.courseInfo+c.coursePlan);
+                        ele.append("<li>\n" +
+                            "\t\t\t\t\t<div class=\"li_box1\">\n" +
+                            "\t\t\t\t\t\t <input type=\"checkbox\" class=\"li_box_che\">\n" +
+                            "\t\t\t\t\t\t <p class=\"li_box_p\"></p>\n" +
+                            "\t\t\t\t\t</div>\n" +
+                            "\t\t\t\t\t<div class=\"li_box2\">\n" +
+                            "\t\t\t\t\t\t<div class=\"input-group\" style=\"float: right;width: 30%;\">\n" +
+                            "\t\t\t\t\t\t    <input type=\"text\" style=\"float: right;text-align: center;\" class=\"form-control input-mini\" value=\"Score:9.0\" readonly=\"readonly\">" +
+                            "<span data-id=\"" + c.courseID + "\" class=\"input-group-addon btn btn-primary\">Details</span>\n" +
+                            "\t\t\t\t\t\t</div>\n" +
+                            "\t\t\t\t\t\t<p contenteditable=\"true\" class=\"box_div_p\">"
+                            + c.courseInfo
+                            + "</p>\n" +
+                            "\t\t\t\t\t</div>\n" +
+                            "\t\t\t\t</li>");
+                    });
+                } else
+                    alert(ret.info);
+            });
+    };
+
     $scope.init_courses_list = function() {
         $http.get(serverUrl+"/course/manage",{params:{userToken: window.localStorage['userToken']}})
         .success(function(ret) {
             var ele = angular.element(document.querySelector("#coursesList>ul"));
+            ele.empty();
 
             if (ret.status) {
                 angular.forEach(ret.courses, function(c) {
+                    // alert(c.courseInfo+c.coursePlan);
                     ele.append("<li>\n" +
                         "\t\t\t\t\t<div class=\"li_box1\">\n" +
                         "\t\t\t\t\t\t <input type=\"checkbox\" class=\"li_box_che\">\n" +
@@ -24,8 +56,8 @@ app.controller("courses_ctrl", ['$scope', '$rootScope', '$http', function($scope
                         "\t\t\t\t\t\t    <input type=\"text\" style=\"float: right;text-align: center;\" class=\"form-control input-mini\" value=\"Score:9.0\" readonly=\"readonly\">" +
                         "<span data-id=\"" + c.courseID + "\" class=\"input-group-addon btn btn-primary\">Details</span>\n" +
                         "\t\t\t\t\t\t</div>\n" +
-                        "\t\t\t\t\t\t<p contenteditable=\"true\" class=\"box_div_p\">" +
-                        +c.courseInfo
+                        "\t\t\t\t\t\t<p contenteditable=\"true\" class=\"box_div_p\">"
+                        + c.courseInfo
                         + "</p>\n" +
                         "\t\t\t\t\t</div>\n" +
                         "\t\t\t\t</li>");
