02f17de984e88d5c371c6c6b1277409e48718391
zukihxy <14302010050@fudan.edu.cn>
2017-11-22 13:49:45 +0800

    refined sign up and sign in

--- web/js/Sign.js
+++ web/js/Sign.js
@@ -6,32 +6,39 @@ app.controller('sign_ctrl', ['$scope', '$rootScope', '$http', function($scope, $
             return $.param(data);
         }
     };
+
     /*
     TODO:
     check school ID and password format
      */
-    $scope.sign_up = function(id,acc,pwd) {
+    $scope.sign_up = function(id,acc,pwd,repwd) {
+        if (pwd != repwd)
+            alert("Re-entered password should be the same as Password! @.@");
         // alert(id+" "+acc+" "+pwd);
-        $http.post(serverUrl+"/sign_up",{schoolID: id, account:acc, password: pwd},postCfg)
-            .success(function(ret){
-                if (ret.status)
-                    alert("Sign up succeed~");
-                else
-                    alert(ret.info);
-            });
+        else {
+            $http.post(serverUrl+"/sign_up",{schoolID: id, account:acc, password: pwd},postCfg)
+                .success(function(ret){
+                    if (ret.status)
+                        alert("Sign up succeed~ ^_^");
+                    else
+                        alert(ret.info);
+                });
+        }
     }
 
     $scope.sign_in = function(acc,pwd) {
         window.localStorage['userToken'] = "hxy";
+        window.localStorage['type'] = 'Teacher';
         window.location.href = "Courses.html";
         window.event.returnValue=false;
 
         // $http.post(serverUrl+"/sign_in",{account:acc, password:pwd},postCfg)
         //     .success(function(ret) {
         //         if (ret.status) {
-        //             window.localStorage['userToken'] = "hxy";
-        //             window.location.href = "Courses.html";
-        //             window.event.returnValue=false;
+        //             window.localStorage['userToken'] = ret.userToken;
+        //             window.localStorage['type'] = ret.type;
+        //             window.location.href = ret.type + ".html";
+        //             window.event.returnValue = false;
         //         } else
         //             alert(ret.info);
         //     });
