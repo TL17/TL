df6ef77701abe2b58bb4514c562fe26c6de10268
pritom <naitiq@yahoo.com>
2017-11-30 14:38:34 +0800

    course evaluation(db table is not ready yet)

--- src/main/java/service/CourseEvaluation.java
+++ src/main/java/service/CourseEvaluation.java
@@ -4,11 +4,15 @@ import entity.service.Course;
 import entity.service.Evaluation;
 import entity.service.Status;
 import net.sf.json.JSONObject;
+import util.db.DBConnect;
 
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLDecoder;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(name="CourseEvaluation", urlPatterns="/course/evaluation/*")
 public class CourseEvaluation extends javax.servlet.http.HttpServlet {
@@ -17,6 +21,13 @@ public class CourseEvaluation extends javax.servlet.http.HttpServlet {
 //        doPost(request,response);
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setContentType("text/html;charset=utf-8");
+
+        Status status = new Status();
+        String url = "jdbc:mysql://123.207.6.234:3306/tl?useSSL=false&serverTimezone=UTC";
+        String user = "root";
+        String dbPwd = "root";
+
+        DBConnect dbConnect = new DBConnect(url, user, dbPwd);
 
         String URI = request.getRequestURI();
         int index = URI.lastIndexOf('/');
@@ -34,27 +45,40 @@ public class CourseEvaluation extends javax.servlet.http.HttpServlet {
             userToken = "";
         }
 
+        //here is the sql statement
+        String querySting = "SELECT * FROM course where id=?";
+        PreparedStatement preparedStatement = dbConnect.prepareStatement(querySting);
         JSONObject jsonRet;
         if (nan||courseIDStr.equals("")||courseIDStr.equals("evaluation")||userToken.equals("")) {
-            Status status = new Status();
+//            Status status = new Status();
             status.setStatus(false);
             status.setInfo("空参数");
             jsonRet = JSONObject.fromObject(status);
             Evaluation[] evaluations = new Evaluation[0];
             jsonRet.put("evaluations",evaluations);
         } else {
-            int courseID = Integer.parseInt(courseIDStr);
-            Status status = new Status();
-            status.setStatus(true);
-            status.setInfo("获取评教成功");
-            jsonRet = JSONObject.fromObject(status);
-            Evaluation[] evaluations = new Evaluation[5];
-            for (int i=0;i<evaluations.length;i++) {
-                evaluations[i] = new Evaluation();
-                evaluations[i].setScore(courseID+".0");
-                evaluations[i].setComment(courseID+"课的评论"+(i+1));
+            try {
+                ResultSet rs = preparedStatement.executeQuery();
+                int courseID = Integer.parseInt(courseIDStr);
+//                Status status = new Status();
+                status.setStatus(true);
+                status.setInfo("获取评教成功");
+                jsonRet = JSONObject.fromObject(status);
+                Evaluation[] evaluations = new Evaluation[5];
+                for (int i = 0; i < evaluations.length; i++) {
+                    evaluations[i] = new Evaluation();
+                    evaluations[i].setScore(rs.getString("score") + ".0");
+                    evaluations[i].setComment(rs.getString("comment") + "课的评论" + (i + 1));
+                }
+                jsonRet.put("evaluations", evaluations);
+            } catch (SQLException e){
+                status.setStatus(false);
+                status.setInfo("空参数 "+ e.getMessage());
+                jsonRet = JSONObject.fromObject(status);
+                Evaluation[] evaluations = new Evaluation[0];
+                jsonRet.put("evaluations",evaluations);
+                e.printStackTrace();
             }
-            jsonRet.put("evaluations",evaluations);
         }
 
         PrintWriter out = response.getWriter();
@@ -65,6 +89,13 @@ public class CourseEvaluation extends javax.servlet.http.HttpServlet {
         //doGet(request,response);
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setContentType("text/html;charset=utf-8");
+
+        Status status = new Status();
+        String url = "jdbc:mysql://123.207.6.234:3306/tl?useSSL=false&serverTimezone=UTC";
+        String user = "root";
+        String dbPwd = "root";
+
+        DBConnect dbConnect = new DBConnect(url, user, dbPwd);
 
         String URI = request.getRequestURI();
         int index = URI.lastIndexOf('/');
@@ -90,18 +121,32 @@ public class CourseEvaluation extends javax.servlet.http.HttpServlet {
             comment = "";
         }
 
+        //here is the sql statement
+        String querySting = "INSERT INTO evauation (id, score, comment) VALUES (?,?,?) ";
+        PreparedStatement preparedStatement = dbConnect.prepareStatement(querySting);
         JSONObject jsonRet;
         if (nan||courseIDStr.equals("")||courseIDStr.equals("evaluation")||userToken.equals("")||score.equals("")||comment.equals("")) {
-            Status status = new Status();
+//            Status status = new Status();
             status.setStatus(false);
             status.setInfo("空参数");
             jsonRet = JSONObject.fromObject(status);
         } else {
-            int courseID = Integer.parseInt(courseIDStr);
-            Status status = new Status();
-            status.setStatus(true);
-            status.setInfo("评教成功");
-            jsonRet = JSONObject.fromObject(status);
+            try {
+                preparedStatement.setString(1,userToken);
+                preparedStatement.setString(2,score);
+                preparedStatement.setString(3,comment);
+                preparedStatement.executeQuery();
+                int courseID = Integer.parseInt(courseIDStr);
+                //            Status status = new Status();
+                status.setStatus(true);
+                status.setInfo("评教成功");
+                jsonRet = JSONObject.fromObject(status);
+            } catch (SQLException e){
+                status.setStatus(false);
+                status.setInfo("空参数 "+e.getMessage());
+                jsonRet = JSONObject.fromObject(status);
+                e.printStackTrace();
+            }
         }
 
         PrintWriter out = response.getWriter();
